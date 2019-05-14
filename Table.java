f8c9f5d420afe62e048fe8f03a954643ffdd19d6
yinkaguti <olayinka2good@yahoo.co.uk>
2019-05-14 16:40:38 +0100

    first major push

---
+++ Table.java
@@ -0,0 +1,18 @@
+// Fig. 2.4: Welcome3.java
+// Printing multiple lines of text with a single statement.
+
+public class Table {
+    //main method begins execution of Java application
+    public static void main(String[] args) {
+        System.out.println("       1");
+        System.out.println("      1 2");
+        System.out.println("     1 2 3");
+        System.out.println("    1 2 3 4");
+        System.out.println("   1 2 3 4 5");
+        System.out.println("  1 2 3 4 5 6");
+        System.out.println(" 1 2 3 4 5 6 7");
+        System.out.println("Account HOlder\tType of Account\tAccount Number\nOlayinka Oloko\t Savings\t  0000000000");
+        System.out.print("baba");
+        System.out.print("tunde");
+    } // end method main
+}  // end class Welcome3
